de31337daf3fe89cf0eb033b7bf7449d063611cc
Voxell <voxell.technologies@gmail.com>
2021-01-26 09:31:48 +0800

    bullet now faces the direction the player is shooting at!

--- Assets/Scripts/Player/GunMec.cs
+++ Assets/Scripts/Player/GunMec.cs
@@ -18,29 +18,32 @@ public class GunMec : MonoBehaviour
   void Update()
   {
     // moving hand based on mouse
+
+    // mouse point in world position
     Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    // mouse point relative to player
     Vector2 mousePos = mouseWorldPos - new Vector2(transform.position.x, transform.position.y);
+
     float pointAngle = Mathf.Atan2(mousePos.y, mousePos.x)* Mathf.Rad2Deg;
     arm.transform.rotation = Quaternion.Euler(0, 0, pointAngle);
-
     cursor.transform.position = mouseWorldPos;
 
-      // right0, up90, left180/-180, down-90;
+    // right0, up90, left180/-180, down-90;
     if(pointAngle >= 90 || pointAngle <= -90)
-      {
-        transform.eulerAngles = new Vector3(0, 180, 0);
-        mousePos.x = -mousePos.x; //fix flipping bug
-      }else
-      {
-        transform.eulerAngles = Vector3.zero;
-      }
+    {
+      transform.eulerAngles = new Vector3(0, 180, 0);
+      mousePos.x = -mousePos.x; //fix flipping bug
+    }else
+    {
+      transform.eulerAngles = Vector3.zero;
+    }
     // shooting
     if(Input.GetKeyDown(shoot) && bulletCount > 0)
     {
       bulletCount--;
 
       Rigidbody2D bullets;
-      bullets = Instantiate(bulletPrefab, firepoint.position, Quaternion.identity);
+      bullets = Instantiate(bulletPrefab, firepoint.position, Quaternion.Euler(0, 0, pointAngle));
       bullets.velocity = transform.TransformDirection(mousePos.normalized)*bulletspeed;
     }
 

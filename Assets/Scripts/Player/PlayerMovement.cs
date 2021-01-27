7deb7ea3e701443f0c9e43f48e1ef036708263f6
Voxell <voxell.technologies@gmail.com>
2021-01-27 17:46:10 +0800

    dash movement implemented, working on shader dash effect

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,12 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  [HideInInspector]
-  public float horizontal;
-  [HideInInspector]
-  public float vertical;
   public Animator animator;
   public float movementMultiplier = 0.1f;
+  public float dashForce = 100;
   [HideInInspector]
   public Rigidbody2D rb;
   // Start is called before the first frame update
@@ -19,27 +16,25 @@ public class PlayerMovement : MonoBehaviour
   {
     if(GunMec.bulletCount > 0)
     {
-    horizontal = Input.GetAxis("Horizontal")*movementMultiplier;
-    vertical = Input.GetAxis("Vertical")*movementMultiplier;
-    rb.AddForce(new Vector3(0, vertical, 0));
-    rb.AddForce(new Vector3(horizontal, 0, 0));
+      float horizontal = Input.GetAxis("Horizontal");
+      float vertical = Input.GetAxis("Vertical");
 
+      Vector3 totalForce = new Vector3(horizontal, vertical, 0)*movementMultiplier;
+
+      animator.SetFloat("speed", Mathf.Min(rb.velocity.magnitude, 1.0f));
+
+      if (Input.GetKeyDown(KeyCode.Space)) totalForce *= dashForce;
+
+      rb.AddForce(totalForce);
       animator.SetFloat("speed", rb.velocity.magnitude);
-    }
-    
-    animator.SetFloat("speed", Mathf.Max(rb.velocity.magnitude, 1.0f));
 
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      // perform dash
+      // // how does this work wtf
+      // if(Mathf.Abs(horizontal) + Mathf.Abs(vertical) == 0f)
+      // {
+      //   SoundManager.PlaySE("walking");
+      // }
+      // //map boundaries
     }
-
-    // how does this work wtf
-    if(Mathf.Abs(horizontal) + Mathf.Abs(vertical) == 0f)
-      {
-        SoundManager.PlaySE("walking");
-      }
-    //map boundaries
   }
 
   void OnCollisionEnter2D(Collision2D col)

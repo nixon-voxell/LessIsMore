1c96e02cfdacd6c88b0a09bb561f9f000b5eb619
Vincexodus <vincent4552@gmail.com>
2021-01-29 21:53:57 +0800

    level preview cam

--- Assets/Scripts/Camera/TrackingCam.cs
+++ Assets/Scripts/Camera/TrackingCam.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class TrackingCam : MonoBehaviour
 {
+  public Camera cam;
+  public float MapcameraSize;
+  public float realCamSize = 15f;
   public Vector3 offset;
   public Transform player;
   public float camspeed = 0.125f;
+  public Vector3 mapPos;
+  void Start()
+  {
+    cam.orthographicSize = MapcameraSize;
+    transform.position = mapPos;
+  }
   void FixedUpdate()
+  {
+    Invoke("PlayerFollow",2f);
+    cam.orthographicSize = Mathf.Lerp(MapcameraSize,realCamSize,0.1f);
+  }
+
+  void PlayerFollow()
   {
     Vector3 desiredPos = player.position + offset;
     Vector3 smoothcam = Vector3.Lerp(transform.position, desiredPos, camspeed);
